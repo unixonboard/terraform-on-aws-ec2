6de6ae21258f2a5b9701a04bd7a28be9a45fadc6
unixonboard <unixonboard@gmail.com>
2026-09-17 11:39:24 -0500

    04

--- 03-Terraform-Settings-Providers-Resources/terraform-manifests/c2-ec2instance.tf
+++ 03-Terraform-Settings-Providers-Resources/terraform-manifests/c2-ec2instance.tf
@@ -1,6 +1,6 @@
 # Resource: EC2 Instance
 resource "aws_instance" "myec2vm" {
-  ami = "ami-0742b4e673072066f"
+  ami = "ami-0e34b50e714a297f1"
   instance_type = "t3.micro"
   user_data = file("${path.module}/app1-install.sh")
   tags = {
